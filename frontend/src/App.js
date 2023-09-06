1047a936d12af35fc854614f7308d267fb0da9e8
Eric Lee Ying Yao <e0694459@u.nus.edu>
2023-09-06 19:20:30 +0800

    Add react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import QuestionBank from "./QuestionBank";
+
+function App() {
+  return (
+    <>
+      <QuestionBank />
+      <QuestionBank />
+    </>
+  );
+}
+
+export default App;
