00d4b1b487c2b007c4775e34323b9a1c557a8944
Eric Lee Ying Yao <e0694459@u.nus.edu>
2023-09-08 19:27:57 +0800

    Add materialui and make table for questions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import QuestionBank from "./QuestionBank";
+import QuestionBank from "./questions/QuestionBank";
 
 function App() {
   return (
---
+++ frontend/src/questions/QuestionBank.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Question from "./Question";
+import { setQuestionsLS, getQuestionsLS } from "./utils/QuestionQueries";
+import { v4 as uuidv4 } from "uuid";
+import { DataGrid } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "title", headerName: "Title", width: 70 },
+  { field: "category", headerName: "Category", width: 200 },
+  { field: "complexity", headerName: "Complexity", width: 130 },
+];
+
+export default function QuestionBank() {
+  useEffect(() => {
+    setQuestionsLS();
+  }, []);
+
+  const [questions, setQuestions] = useState(() => {
+    return (
+      getQuestionsLS().map((question) => {
+        return { ...question, id: uuidv4() };
+      }) || []
+    );
+  });
+
+  return (
+    <DataGrid
+      rows={questions}
+      columns={columns}
+      initialState={{
+        pagination: {
+          paginationModel: { page: 0, pageSize: 5 },
+        },
+      }}
+      pageSizeOptions={[5, 10]}
+      checkboxSelection
+    />
+  );
+  // return questions.map((question) => (
+  //   <Question key={uuidv4()} question={question} />
+  // ));
+}
