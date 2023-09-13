2384cbdd72215c1229de0fc9af548beeea90a4b2
PangKuangWei <97268525+PangKuangWei@users.noreply.github.com>
2023-09-14 02:36:59 +0800

    Fix styling for no desc

--- frontend/src/questions/QuestionDescription.js
+++ frontend/src/questions/QuestionDescription.js
@@ -8,17 +8,17 @@ export const QuestionDescription = () => {
   const question = getQuestionsLS().find((qn) => qn.title === questionTitle);
   if (question.description === undefined) {
     return (
-      <div className="bg-white">
-        <h1 className="text-3xl font-bold mb-4">{question.title}</h1>
-        <div className="mb-2">
+      <div className="bg-white p-4">
+        <h1 className="text-3xl font-bold mb-4 p-4 rounded shadow-lg">{question.title}</h1>
+        <div className="mb-2 p-4 rounded shadow-lg">
           <p className="text-gray-700 font-bold">Category:</p>
           <p className="text-xl">{question.category}</p>
         </div>
-        <div className="mb-2">
+        <div className="mb-2 p-4 rounded shadow-lg">
           <p className="text-gray-700 font-bold">Complexity:</p>
           <p className="text-lg">{question.complexity}</p>
         </div>
-        <div className="mb-2">
+        <div className="mb-2 p-4 rounded shadow-lg">
           <p className="text-gray-700 font-bold">Description:</p>
           <p className="text-gray-700">{"No Description Available"}</p>
         </div>
