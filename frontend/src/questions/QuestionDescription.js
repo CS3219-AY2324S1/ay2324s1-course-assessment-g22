62af61d442b41b5bbd7ead1f54858cbd8cdd61e2
PangKuangWei <97268525+PangKuangWei@users.noreply.github.com>
2023-09-14 01:13:16 +0800

    Change data structure

--- frontend/src/questions/QuestionDescription.js
+++ frontend/src/questions/QuestionDescription.js
@@ -1,13 +1,52 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { questionDescriptions } from "./utils/QuestionData";
-import parse from "html-react-parser";
+import { questionData } from "./utils/QuestionData";
 
 export const QuestionDescription = () => {
   const urlPathQnId = useParams();
-  const questionId = urlPathQnId.title;
-  const question = questionDescriptions.find((qn) => qn.title === questionId);
-  return <div>{parse(question.description)}</div>;
+  const questionTitle = urlPathQnId.title;
+  const question = questionData.find((qn) => qn.title === questionTitle);
+  if (question.description === undefined) {
+    return (
+      <div className="bg-white p-4 rounded shadow-lg">
+        <h1 className="text-3xl font-bold mb-4">{question.title}</h1>
+        <div className="mb-2">
+          <p className="text-gray-700 font-bold">Category:</p>
+          <p className="text-xl">{question.category}</p>
+        </div>
+        <div className="mb-2">
+          <p className="text-gray-700 font-bold">Complexity:</p>
+          <p className="text-lg">{question.complexity}</p>
+        </div>
+        <div className="mb-2">
+          <p className="text-gray-700 font-bold">Description:</p>
+          <p className="text-gray-700">{"No Description Available"}</p>
+        </div>
+      </div>
+    );
+  }
+  const descriptionParas = question.description.split("\n");
+  return (
+    <div className="bg-white p-4 rounded shadow-lg">
+      <h1 className="text-3xl font-bold mb-4">{question.title}</h1>
+      <div className="mb-2">
+        <p className="text-gray-700 font-bold">Category:</p>
+        <p className="text-xl">{question.category}</p>
+      </div>
+      <div className="mb-2">
+        <p className="text-gray-700 font-bold">Complexity:</p>
+        <p className="text-lg">{question.complexity}</p>
+      </div>
+      <div className="mb-2">
+        <p className="text-gray-700 font-bold">Description:</p>
+        <div className="text-gray-700">
+          {descriptionParas.map((para, index) => (
+            <p key={index}>{para}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QuestionDescription;
