c56abdd20de382d62324ff5acdbbc40daf8571c1
Eric Lee Ying Yao <e0694459@u.nus.edu>
2023-09-19 12:52:17 +0800

    use commonjs notation and test for mongodb connection

--- mongodb/config/dbConnect.js
+++ mongodb/config/dbConnect.js
@@ -1,10 +1,13 @@
-import { username, password, clusterName, databaseName } from "./dbAdmin";
-
 const mongoose = require("mongoose");
 const express = require("express");
-const Router = require("./routes");
+const router = require("../routes/routes");
+const dbAdmin = require("./dbAdmin");
 
 const LISTEN_PORT_NUMBER = 3000;
+const username = dbAdmin.username;
+const password = dbAdmin.password;
+const clusterName = dbAdmin.clusterName;
+const databaseName = dbAdmin.databaseName;
 
 const app = express();
 app.use(express.json());
@@ -12,12 +15,7 @@ app.use(express.json());
 // Initial connection to database
 mongoose
   .connect(
-    `mongodb+srv://${username}:${password}@${clusterName}.rwipe4m.mongodb.net/${databaseName}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    }
+    `mongodb+srv://${username}:${password}@${clusterName}.rwipe4m.mongodb.net/${databaseName}?retryWrites=true&w=majority`
   )
   .catch((error) => console.log(error));
 
@@ -26,7 +24,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection failed: "));
 db.once("open", () => console.log("Connected successfully to MongoDB"));
 
-app.use(Router);
+app.use(router);
 app.listen(LISTEN_PORT_NUMBER, () => {
   console.log("Connected to port 3000");
 });
--- mongodb/routes/routes.js
+++ mongodb/routes/routes.js
@@ -2,6 +2,25 @@ const express = require("express");
 const questionModels = require("../config/dbModels.js");
 const app = express();
 
-// app.post("/add_qn", async(req, res) => {
-//     const qn = new
-// })
+app.post("/add_qn", async (req, res) => {
+  const qn = new questionModels(req.body);
+
+  try {
+    await qn.save();
+    res.send(qn);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get("/get_qn", async (req, res) => {
+  const qn = await questionModels.find({});
+
+  try {
+    res.send(qn);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = app;
