581b54173e498eb993a957c6c6ece24b00f46ab0
Eugene-Ong-W-X <e0725338@u.nus.edu>
2023-10-21 15:40:16 +0800

    Add dependency for User Service, Add SQL scripts

--- backend/user-service/sqlFiles/userAccountsSchema.sql
+++ backend/user-service/sqlFiles/userAccountsSchema.sql
@@ -8,3 +8,15 @@ CREATE TABLE IF NOT EXISTS userAccounts (
     created_at TIMESTAMPTZ DEFAULT current_timestamp,
     role VARCHAR(50) DEFAULT 'user' NOT NULL
 );
+
+INSERT INTO userAccounts (username, email, password, firstname, lastname) VALUES
+('user1', 'user1@example.com', 'hashed_password_1', 'John', 'Doe'),
+('user2', 'user2@example.com', 'hashed_password_2', 'Alice', 'Smith'),
+('user3', 'user3@example.com', 'hashed_password_3', 'Bob', 'Johnson'),
+('user4', 'user4@example.com', 'hashed_password_4', 'Emma', 'Williams'),
+('user5', 'user5@example.com', 'hashed_password_5', 'Michael', 'Brown');
+
+INSERT INTO userAccounts (username, email, password, firstname, lastname, role) VALUES
+('maintainer', 'maintainer1@example.com', 'hashed_password_1', 'Joe', 'The Maintainer', 'maintainer'),
+('m', 'maintainer2@example.com', 'm', 'Steve', 'The Maintainer', 'maintainer'),
+('admin', 'maintainer3@example.com', 'a', 'Martin', 'The Maintainer', 'maintainer');
